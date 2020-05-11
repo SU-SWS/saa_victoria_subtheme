d47fedd9fb2cbf725813b983eaec65f5ba02693c
Katria Lesser <Katria@stanford.edu>
2020-05-11 08:31:39 -0600

    ADAPT-295: Configured tailwind to be more to our styles and settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
   theme: {
     boxShadow: {
+      'flat': 'none',
+      sm: '0px 1px 2px rgba(0, 0, 0, 0.25)',
       default: '5px 5px 20px rgba(0, 0, 0, 0.1)',
       md: '0px 10px 20px rgba(0, 0, 0, 0.15), 0px 6px 6px rgba(0, 0, 0, 0.2)',
       lg: '0px 14px 28px rgba(0, 0, 0, 0.2), 0px 5px 10px rgba(0, 0, 0, 0.22)',
       xl: '0px 19px 38px rgba(0, 0, 0, 0.25), 0px 5px 12px rgba(0, 0, 0, 0.18)',
-      'none': 'none',
     },
     fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphik', 'sans-serif'],
+      display: ['Calluna', 'serif'],
+      body: ['Proxima Nova', 'sans-serif'],
     },
     screens: {
       tablet: '768px',
-      desktop: '1024px',
+      desktop: '1024px', // might need to change
     },
     colors: {
       primary: {
-        100: '#ebf8ff',
+        100: '#ebf8ff',  // need to figure out how this is used and which color it should be, could one of these be grayscales?
         300: '#90cdf4',
         500: '#4299e1',
         700: '#2b6cb0',
