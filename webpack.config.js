d43073090a40b8c4f42b12323679c409466458b6
Katria Lesser <katrialesser@users.noreply.github.com>
2020-08-12 14:30:17 -0600

    ADAPT-335  Webpack reorganization (#35)
    * ADAPT-335: Cherrypick orignal webpack reorg.
    
    * Removed commented-out includes
    
    * ADAPT-335: Change final file name and adjust config inclusion.
    
    * ADAPT-335: Moving base font inclusion line.
    
    * ADAPT-000: Fix ul ol style with tailwind override file.
    
    * ADAPT-000: Removed bad color declaration that wasnt nevessary.
    
    * ADAPT-335: Changed from individual files to single css file.
    
    * ADAPT-335: Fixed the fonts inclusion inside base fonts index.
    
    * ADAPT-335: Removed old test.
    
    * ADAPT-335: Removed old comments.
    
    * ADAPT-335: Merged 1.x in to update branch.
    
    * ADAPT-335: Removed decorator class and comments.
    
    * ADAPT-335: Regenerated after merging 1.x in
    
    * ADAPT-335: Rebuilt after merging in 1.x
    
    * ADAPT-335 Recompiled stylesheets
    
    Co-authored-by: Ian Monroe <ian@ianmonroe.com>
    Co-authored-by: Katria Lesser <Katria@stanford.edu>
    Co-authored-by: Katria Lesser <Devbydesign@mindsing.com>
    Co-authored-by: Rebecca Hong <Rebeccahongsf@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -48,13 +48,9 @@ var webpackConfig = {
   devtool: 'source-map',
   // What build?
   entry: {
-    "base":          path.resolve(srcSass, "base/index.scss"),
+    "saa-victoria":  path.resolve(srcSass, "config/index.scss"),
     "scripts":       path.resolve(srcJS,   "scripts.js"),
-    "components":    path.resolve(srcSass, "components/index.scss"),
-    "layout":        path.resolve(srcSass, "layout/index.scss"),
-    "print":         path.resolve(srcSass, "print/index.scss"),
-    "state":         path.resolve(srcSass, "state/index.scss"),
-    "theme":         path.resolve(srcSass, "theme/index.scss"),
+
   },
   // Where put build?
   output: {
