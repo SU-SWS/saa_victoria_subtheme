879d1ae33e795a310fa7d5bff6ed8fa1f63d6b27
Katria Lesser <Katria@stanford.edu>
2020-09-09 14:58:46 -0600

    ADAPT-684: Added id for links to go to.

--- src/associates/src/components/App.jsx
+++ src/associates/src/components/App.jsx
@@ -64,7 +64,7 @@ class App extends React.Component {
     return (
       <main>
         <header>
-          <div name="top" />
+          <div id="associates--top-of-page" name="top" />
           <Counter
             className="counter"
             count={this.state.names ? this.state.names.length : 0}
