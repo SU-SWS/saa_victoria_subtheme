88b6dd34bb528652d394929b5ca76ca1fb5265ba
Nathan Plowman <nathan.plowman@ffwagency.com>
2021-09-23 13:14:58 -0600

    ADAPT-3246: Added no results content and styling for Algolia Search page.

--- dist/js/search-page.js
+++ dist/js/search-page.js
@@ -55679,11 +55679,45 @@ var SearchField = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.forw
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var decanter_react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! decanter-react */ "./node_modules/decanter-react/dist/decanter-react.module.js");
+/* harmony import */ var _heroIcon__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./heroIcon */ "./src/react/components/heroIcon.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
 
 
+var columns = [{
+  heading: 'Popular Topics',
+  links: [{
+    title: 'Reunion 2021',
+    url: 'https://reunion.stanford.edu/'
+  }, {
+    title: 'Programs & Resources for Students',
+    url: 'https://students.alumni.stanford.edu/'
+  }, {
+    title: 'Request your official Stanford transcript',
+    url: 'https://registrar.stanford.edu/students/transcripts/transcripts-alumni'
+  }, {
+    title: 'Stanford Alumni Email',
+    url: 'https://alumni-preview.stanford.edu/perks/email/'
+  }, {
+    title: 'Alumni Membership',
+    url: 'https://alumni-preview.stanford.edu/membership/'
+  }]
+}, {
+  heading: 'Contact Information',
+  links: [{
+    title: 'SAA Customer Service',
+    url: 'https://alumni.stanford.edu/goto/snowcs'
+  }, {
+    title: 'Stanford University Contact Information',
+    url: 'https://www.stanford.edu/contact/'
+  }, {
+    title: 'SAA Departments',
+    url: 'https://alumni-preview.stanford.edu/about/departments/'
+  }]
+}];
+var listClasses = "algolia-search--no-results-list list-none pl-0 children:leading-none";
+var listItemClasses = "algolia-search--no-results-list-item block text-left mb-6";
+var linkClasses = "algolia-search--no-results-link flex w-fit transition-colors no-underline underline-offset hover:underline focus:underline";
 
 var SearchNoResults = function SearchNoResults(_ref) {
   var heading = _ref.heading,
@@ -55694,7 +55728,26 @@ var SearchNoResults = function SearchNoResults(_ref) {
     className: "algolia-search--no-results-heading"
   }, heading), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
     className: "algolia-search--no-results-body"
-  }, body));
+  }, body), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("h3", {
+    className: "algolia-search--no-results-suggestions-heading mt-40 mb-12 md:mb-20"
+  }, "Consider Browsing by Category:"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "w-full grid"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-y-16 md:gap-x-8 xl:gap-x-12 justify-items-start grid-gap items-start"
+  }, columns.map(function (column) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("h4", {
+      className: "algolia-search--no-results-suggestions-subheading mb-16"
+    }, column.heading), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("ul", {
+      class: listClasses
+    }, column.links.map(function (item) {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("li", {
+        className: listItemClasses
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("a", {
+        href: item.url,
+        class: linkClasses
+      }, item.title));
+    })));
+  })))));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (SearchNoResults);
--- src/react/components/searchNoResults.js
+++ src/react/components/searchNoResults.js
@@ -1,5 +1,30 @@
-import { Heading } from "decanter-react";
+import HeroIcon from "./heroIcon";
 import React from "react";
+
+const columns = [
+  {
+    heading: 'Popular Topics',
+    links: [
+      {title: 'Reunion 2021', url: 'https://reunion.stanford.edu/'},
+      {title: 'Programs & Resources for Students', url: 'https://students.alumni.stanford.edu/'},
+      {title: 'Request your official Stanford transcript', url: 'https://registrar.stanford.edu/students/transcripts/transcripts-alumni'},
+      {title: 'Stanford Alumni Email', url: 'https://alumni-preview.stanford.edu/perks/email/'},
+      {title: 'Alumni Membership', url: 'https://alumni-preview.stanford.edu/membership/'},
+    ]
+  },
+  {
+    heading: 'Contact Information',
+    links: [
+      {title: 'SAA Customer Service', url: 'https://alumni.stanford.edu/goto/snowcs'},
+      {title: 'Stanford University Contact Information', url: 'https://www.stanford.edu/contact/'},
+      {title: 'SAA Departments', url: 'https://alumni-preview.stanford.edu/about/departments/'},
+    ]
+  }
+]
+
+const listClasses = "algolia-search--no-results-list list-none pl-0 children:leading-none";
+const listItemClasses = "algolia-search--no-results-list-item block text-left mb-6";
+const linkClasses = "algolia-search--no-results-link flex w-fit transition-colors no-underline underline-offset hover:underline focus:underline";
 
 const SearchNoResults = ({ heading, body }) => (
   <div className="algolia-search--no-results">
@@ -7,6 +32,29 @@ const SearchNoResults = ({ heading, body }) => (
       {heading}
     </h2>
     <p className="algolia-search--no-results-body">{body}</p>
+    <div>
+      <h3 className="algolia-search--no-results-suggestions-heading mt-40 mb-12 md:mb-20">Consider Browsing by Category:</h3>
+      <div className="w-full grid">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-y-16 md:gap-x-8 xl:gap-x-12 justify-items-start grid-gap items-start">
+          {columns.map((column) => (
+            <div>
+              <h4 className="algolia-search--no-results-suggestions-subheading mb-16">
+                {column.heading}
+              </h4>
+              <ul class={listClasses}>
+                {column.links.map((item) => (
+                  <li className={listItemClasses}>
+                    <a href={item.url} class={linkClasses}>
+                      {item.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
